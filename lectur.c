80da9951196502021e1a78b22898244aeddd7602
evaneldred <156348320+evaneldred@users.noreply.github.com>
2024-01-30 11:54:02 -0500

    Add files via upload

---
+++ lectur.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    double x = 16;
+    printf("Hello, world\n");
+    printf("The squar root of %f is %f.\n", x, sqrt(x));
+    
+    return(EXIT_SUCCESS);
+}
